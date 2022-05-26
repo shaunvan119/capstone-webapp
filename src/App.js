a744853e4579e879658e1c67273ea14ca6bd82d1
shaunvan119 <shaunvan@live.com.au>
2022-05-26 18:48:11 +1000

    fixed switch error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
+import {Routes, Route} from "react-router-dom"
 import Header from "./components/Header";
+import Cart from "./pages/Cart";
+import Photos from "./pages/Photos";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+    
+      <Routes>
+          <Route path="/" element={<Photos />}/>
+          <Route path="/cart" element={<Cart />}/>    
+      </Routes>
+   
+    
     </div>
   );
 }
