2e79fb60906c9e395f0a583e547ba77c0066357e
shaunvan119 <shaunvan@live.com.au>
2022-05-27 10:07:02 +1000

    context varible

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,14 @@
-import React from "react"
+import React, {useState} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
+    const [ allPhotos, setAllPhotos] = useState([]);
+     
+console.log(allPhotos)
     return (
-        <Context.Provider value="">
+        
+        <Context.Provider value={{allPhotos}}>
             {children}
         </Context.Provider>
     )
