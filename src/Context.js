fd6faf64aab37cfc4fd92dac424743865b0a23e0
shaunvan119 <shaunvan@live.com.au>
2022-05-27 11:06:11 +1000

    saved api array of data that comes back to state

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,21 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [ allPhotos, setAllPhotos] = useState([]);
      
-console.log(allPhotos)
+
+const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+
+useEffect(() => {
+    fetch(url) // fetch's the data
+    .then(response => response.json()) // handles the response
+    .then(data => setAllPhotos(data))
+  },[]);
+
+  console.log(allPhotos)
+
     return (
         
         <Context.Provider value={{allPhotos}}>
